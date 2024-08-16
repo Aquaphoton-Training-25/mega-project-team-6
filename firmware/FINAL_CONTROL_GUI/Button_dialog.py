b204875810cec358a8c486152593d4c356821002
Yassin Khadrawy <yassinkhaled246@gmail.com>
2024-08-16 04:30:06 +0300

    Update Button_dialog.py

--- firmware/FINAL_CONTROL_GUI/Button_dialog.py
+++ firmware/FINAL_CONTROL_GUI/Button_dialog.py
@@ -1,11 +1,11 @@
-# Button_dialog.py
 from PyQt6 import QtCore, QtGui, QtWidgets
-
-
+                                                    #########################################################
+                                                    ################   HI AQUAPHOTON   ######################
+                                                    #########################################################
 class Ui_Button_dialog(object):
     def setupUi(self, Button_dialog):
         Button_dialog.setObjectName("Button_dialog")
-        Button_dialog.resize(4000, 3000)
+        Button_dialog.resize(800, 600)
         Button_dialog.setStyleSheet("background-color: rgb(0, 0, 34);")
 
         self.dial = QtWidgets.QDial(parent=Button_dialog)
@@ -13,49 +13,65 @@ class Ui_Button_dialog(object):
         self.dial.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.dial.setObjectName("dial")
 
+        #####################################################
+        # Down Button with Image
+        #####################################################
+
         self.down_pushButton = QtWidgets.QPushButton(parent=Button_dialog)
         self.down_pushButton.setGeometry(QtCore.QRect(230, 240, 61, 121))
         self.down_pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.down_pushButton.setText("")
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("images/down.webp"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
-        self.down_pushButton.setIcon(icon)
+        down_icon = QtGui.QIcon(QtGui.QPixmap(r"C:\Users\KimoStore\Downloads\FINAL_CONTROL_GUI\images\images\down.png"))
+        self.down_pushButton.setIcon(down_icon)
         self.down_pushButton.setIconSize(QtCore.QSize(80, 65))
         self.down_pushButton.setCheckable(True)
         self.down_pushButton.setAutoExclusive(True)
         self.down_pushButton.setObjectName("down_pushButton")
 
+
+        #####################################################
+        # Right Button with Image
+        #####################################################
+
+
         self.right_pushButton = QtWidgets.QPushButton(parent=Button_dialog)
         self.right_pushButton.setGeometry(QtCore.QRect(340, 130, 121, 61))
         self.right_pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.right_pushButton.setText("")
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("images/right.jfif"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
-        self.right_pushButton.setIcon(icon1)
+        right_icon = QtGui.QIcon(QtGui.QPixmap(r"C:\Users\KimoStore\Downloads\FINAL_CONTROL_GUI\images\images\right.png"))
+        self.right_pushButton.setIcon(right_icon)
         self.right_pushButton.setIconSize(QtCore.QSize(80, 65))
         self.right_pushButton.setCheckable(True)
         self.right_pushButton.setAutoExclusive(True)
         self.right_pushButton.setObjectName("right_pushButton")
 
+
+        #####################################################
+        # Up Button with Image
+        #####################################################
+
+
         self.up_pushButton = QtWidgets.QPushButton(parent=Button_dialog)
         self.up_pushButton.setGeometry(QtCore.QRect(230, 0, 61, 121))
         self.up_pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.up_pushButton.setText("")
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("images/up.jfif"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
-        self.up_pushButton.setIcon(icon2)
-        self.up_pushButton.setIconSize(QtCore.QSize(80, 65))
+        up_icon = QtGui.QIcon(QtGui.QPixmap(r"C:\Users\KimoStore\Downloads\FINAL_CONTROL_GUI\images\images\up.png"))
+        self.up_pushButton.setIcon(up_icon)
+        self.up_pushButton.setIconSize(QtCore.QSize(65, 50))
         self.up_pushButton.setCheckable(True)
         self.up_pushButton.setAutoExclusive(True)
         self.up_pushButton.setObjectName("up_pushButton")
+
+        #####################################################
+        # Left Button with Image
+        #####################################################
 
         self.left_pushButton = QtWidgets.QPushButton(parent=Button_dialog)
         self.left_pushButton.setGeometry(QtCore.QRect(90, 130, 121, 61))
         self.left_pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.left_pushButton.setText("")
-        icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap("images/left.png"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
-        self.left_pushButton.setIcon(icon3)
+        left_icon = QtGui.QIcon(QtGui.QPixmap(r"C:\Users\KimoStore\Downloads\FINAL_CONTROL_GUI\images\images\left.png"))
+        self.left_pushButton.setIcon(left_icon)
         self.left_pushButton.setIconSize(QtCore.QSize(80, 65))
         self.left_pushButton.setCheckable(True)
         self.left_pushButton.setAutoExclusive(True)
@@ -68,7 +84,6 @@ class Ui_Button_dialog(object):
         _translate = QtCore.QCoreApplication.translate
         Button_dialog.setWindowTitle(_translate("Button_dialog", "Dialog"))
 
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -77,3 +92,8 @@ if __name__ == "__main__":
     ui.setupUi(Button_dialog)
     Button_dialog.show()
     sys.exit(app.exec())
+
+                                                        #########################################################
+                                                        ################   BYE AQUAPHOTON   #####################
+                                                        #########################################################
+
